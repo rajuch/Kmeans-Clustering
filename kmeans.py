95f99322092bcb4392abf21a6ad4e324346aff1e
rajuch <ch.raju532@gmail.com>
2013-08-27 12:32:40 +0530

    formatting

--- kmeans.py
+++ kmeans.py
@@ -14,6 +14,9 @@ from sklearn.cluster import KMeans
 from time import time
 
 def getRepos():
+    """
+    retrieve the repository ids, languages from the database
+    """
     sql = 'select id, language from repository order by id ASC'
     con = db.getDBConnection()
     rows=db.executeSQL(con, sql);
@@ -25,6 +28,9 @@ def getRepos():
     return repoList
 
 def getCleanKeywords():
+    """
+    retrieve the words, document frequency>5 from the database
+    """
     con = db.getDBConnection()
     sql = 'select word, df from clean_keywords1 where df>5'
     cleanWordRows = db.executeSQL(con, sql)
@@ -38,6 +44,9 @@ def getCleanKeywords():
     return wordDFMap
    
 def preapreData():
+    """
+    prepares the sparse matrix for kmeans
+    """
     con = db.getDBConnection()
     rowCount =0
     global repoLangMap    
@@ -58,15 +67,15 @@ def preapreData():
                 val=wordDFMap[word]
                 index = int(val.split(":")[0])
                 df = int(val.split(":")[1])
-                #print tf * math.log(float(noOfRepos/df)), tf, df, noOfRepos
                 print rowCount, index
-                if index >6000:
-                    print '<====== something is wrong =======>'
                 S[rowCount, index] = tf * math.log(float(noOfRepos/df))
            
         rowCount = rowCount + 1
 
 def getLanguage():
+    """
+    retireves the distinct languages from the database
+    """
     con = db.getDBConnection()
     sql = 'select distinct language from repository'
     res = db.executeSQL(con, sql)
@@ -81,6 +90,42 @@ def getLanguage():
             langMap[lang] = index
             index += 1
 
+def decodeClusterOutput(path,fileName):
+    """
+    decodes the cluster output(binary format) and writes to the new file
+    input: fileName - cluster output file name
+    """
+    repoList=getRepos()
+    a=np.fromfile(path+fileName,dtype=np.int64)
+    count=0
+    clusterMap ={}
+    f = open(path+'newoutput_100','w')
+    for key in a:
+        if key in clusterMap:
+            indexes= clusterMap[key] 
+            clusterMap[key] = indexes + ":" + str(count) 
+        else :
+            clusterMap[key] = str(count)
+        count +=1
+
+    for key in clusterMap.keys():
+        indexes = clusterMap[key]
+        arr = indexes.split(":")
+        string =''
+        for val in arr:
+            if string =='':
+                string= str(repoList[int(val)])
+            else:
+                string= string + ","+str(repoList[int(val)])
+        print string
+        f.write(string)
+        f.write('\n')
+    f.close()
+
+
+"""
+variables declaration
+"""
 t2 = time()        
 repoLangMap = {}
 langMap = {}
@@ -92,31 +137,30 @@ getLanguage()
 noOfRepos = len(repoList)
 noOfKeyWords = len(wordDFMap) + len(langMap)  
 print noOfRepos, noOfKeyWords
+#initialize the sparse matrix with the size of repositories and keywords
 S = dok_matrix((noOfRepos,noOfKeyWords), dtype=float32)
 print 'time taken for preparing data::' + str(time()-t2)
 preapreData() 
 t3 = time()
 print 'time taken for preparing data::' + str(t3-t2)
 t0 = time()
-#km=KMeans(n_clusters=1000, init='k-means++', max_iter=100, n_init=10,
-#                verbose=1, n_jobs=-2)
 
-#output=km.predict(S.toarray)
-#np.ndarray.tofile(output,'/home/raju/Work/clusteroutput')
-#print output
+path = '/home/raju/Work/Cluster/'
+clusterCentroidsFile = 'newclustercentroids_100'
+clusterOutputFile = 'newclusteroutput2_1000'
 
-
-# computing K-Means with K = 2 (2 clusters)
+# computing K-Means with K = 100 (100 clusters)
 print 'shape of array', S.get_shape()
 centroids,_ = kmeans(S.toarray(),100)
-np.ndarray.tofile(centroids,'/home/raju/Work/newclustercentroids_100')
+np.ndarray.tofile(centroids,path+clusterCentroidsFile)
 print centroids
 
 # assign each sample to a cluster
 idx,_ = vq(S.toarray(),centroids)
-np.ndarray.tofile(idx,'/home/raju/Work/newclusteroutput2_1000')
+np.ndarray.tofile(idx,path+clusterOutputFile)
 print idx
 
+decodeClusterOutput(path,clusterOutputFile)
 t1 = time()
 print 'kmeans time::' + str(t1-t0)
 
